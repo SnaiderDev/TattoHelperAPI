d5bf5e32e43272a57a3aabccff8dfcaccef96c16
SnaiderDev <snaiderortega10@gmail.com>
2026-07-17 08:59:16 -0500

    Add zod to the user functions

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,4 +1,3 @@
-import express from "express";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 import pc from "picocolors";
@@ -7,37 +6,56 @@ import z from "zod";
 
 //creacion de un usuario
 export async function createUser(name, email, password) {
-  //declaracion de reglas antes de pasar los datos al modelo de usuario
   const userSchemaValidation = z.object({
-    nameObj: z.string(),
-    emailDataObj: z.string().email().lowercase(),
-    passwordObj: z.string().min(6),
+    name: z.string().min(1),
+    email: z.string().email().transform((val) => val.toLowerCase()),
+    password: z.string().min(6),
   });
   //comprobacion de los datos
-
-  const validationResult = userSchemaValidation.safeParse({
-    nameObj: name,
-    emailObj: email,
-    passwordObj: password,
-  });
+  const validationResult = userSchemaValidation.safeParse({name,email,password});
   if (!validationResult.success) {
-    console.error(pc.yellow(`Error creating user: ${pc.red(validationResult.error)}`));
+    console.error(
+      pc.yellow(`Error creating user: ${pc.red(validationResult.error)}`),
+    );
     return null;
-  } else {
-    console.log(validationResult.data)
-    const { name, email, password } = validationResult.data;
-    console.log(name, emailObj, passwordObj);
-    const newUser = new user({ nameObj, emailObj, passwordObj });
-    return await newUser.save();
   }
-}
-
+  console.log(validationResult.data);
+  //preparacion de los datos para enviarsela al modelo
+  const data = {
+    name: validationResult.data.name,
+    email : validationResult.data.email,
+    password: validationResult.data.password
+  }
+  const newUser = new user({
+    name: data.name,
+    email: data.email,
+    password: data.password
+  });
+  return await newUser.save()
+  }
+   
 //Login y creacio de token de usuario
 export async function loginUser(email, password) {
-  const userMatch = await user.findOne({ email });
+    //esquema de validacion de datos de usuario
+    const userDataValidation = z.object({
+        email: z.string().email().transform((val) => val.toLowerCase()),
+        password: z.string()
+    })
+    const validationResult = userDataValidation.safeParse({email,password});
+    if (!validationResult.success){
+        console.log(pc.yellow(`Error logging in user: ${pc.red(validationResult.error)}`))
+        return null;
+    }
+    //preparacion de la data para su consulta
+    const data = {
+        email : validationResult.data.email,
+        password: validationResult.data.password
+    }
+
+  const userMatch = await user.findOne({ email:data.email });
   if (!userMatch) return null;
 
-  const isMatch = await bcrypt.compare(password, userMatch.password);
+  const isMatch = await bcrypt.compare(data.password, userMatch.password);
   if (!isMatch) return null;
 
   if (!process.env.JWT_SECRET) {
